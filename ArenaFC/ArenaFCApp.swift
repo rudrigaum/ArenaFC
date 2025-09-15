fbae178770f122ddef1b66169379d64d10f77687
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-09-15 09:57:28 -0300

    Initial Commit

---
+++ ArenaFC/ArenaFCApp.swift
@@ -0,0 +1,32 @@
+//
+//  ArenaFCApp.swift
+//  ArenaFC
+//
+//  Created by Rodrigo Cerqueira Reis on 15/09/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ArenaFCApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}

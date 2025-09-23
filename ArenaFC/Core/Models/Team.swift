28b22b74f002ffcd6700593cb0d0f885efeb402d
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-09-23 14:44:56 -0300

    feat(models): Expand Team model for details screen

---
+++ ArenaFC/Core/Models/Team.swift
@@ -0,0 +1,27 @@
+//
+//  Team.swift
+//  ArenaFC
+//
+//  Created by Rodrigo Cerqueira Reis on 15/09/25.
+//
+
+import Foundation
+
+struct APIResponseTeams: Decodable {
+    let response: [TeamDetailsWrapper]
+}
+
+struct TeamDetailsWrapper: Decodable {
+    let team: Team
+    let venue: Venue
+}
+
+struct Team: Decodable, Identifiable, Equatable, Hashable {
+    let id: Int
+    let name: String
+    let code: String?
+    let country: String?
+    let founded: Int?
+    let national: Bool?
+    let logo: URL
+}
